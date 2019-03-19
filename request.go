c3e273384a650ced7f960d2778cb570725fb6898
Sheva Xu <shevaxu@outlook.com>
2019-03-19 18:19:58 +0800

    feat: generalize Get()

--- request.go
+++ request.go
@@ -15,6 +15,7 @@ func HandleResp(r *http.Response, v interface{}) (err error) {
 	if err != nil {
 		return
 	}
+
 	// try error first
 	var ce CanonicalizedError
 	err = json.Unmarshal(bs, &ce)
@@ -28,19 +29,21 @@ func HandleResp(r *http.Response, v interface{}) (err error) {
 		return &ce
 	}
 
-	err = json.Unmarshal(bs, &v)
+	err = json.Unmarshal(bs, v)
 	return
 }
 
-// Get makes a HTTP GET request to the provided url
-// and handles the response.
-func Get(url string, v interface{}) error {
-	r, err := http.Get(url)
-	if r != nil {
+// Get makes a HTTP GET request to the host
+// for the provided API, and marshal the response
+// into the value pointed by resp.
+func Get(cl *http.Client, s Signer, a API, host string, resp interface{}) error {
+	url := host + "?" + s.Sign(a)
+	r, err := cl.Get(url)
+	if r != nil && r.Body != nil {
 		defer r.Body.Close()
 	}
 	if err != nil {
 		return err
 	}
-	return HandleResp(r, &v)
+	return HandleResp(r, resp)
 }

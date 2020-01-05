028a2dbb403f3213f72396a7e32e9329578f73c7
Sheva Xu <shevaxu@outlook.com>
2020-01-05 20:26:29 +0800

    feat (live): add DescribeRecordContent API

--- live/record.go
+++ live/record.go
@@ -8,6 +8,20 @@ import (
 	"github.com/practigo/aliyun"
 )
 
+// DescribeRecordContentAPI returns the API for DescribeLiveStreamRecordContent.
+// https://help.aliyun.com/document_detail/35421.html.
+func DescribeRecordContentAPI(uri StreamURI, start, end time.Time) aliyun.API {
+	a := &api{v: url.Values{}}
+
+	// api-specific
+	a.v.Add("Action", "DescribeLiveStreamRecordContent")
+	fillURI(a.v, uri)
+	a.v.Add("StartTime", aliyun.FormatT(start))
+	a.v.Add("EndTime", aliyun.FormatT(end))
+
+	return a
+}
+
 // DescribeRecordsAPI returns the API for DescribeLiveStreamRecordIndexFiles.
 // https://help.aliyun.com/document_detail/35423.html
 func DescribeRecordsAPI(uri StreamURI, start, end time.Time) aliyun.API {
@@ -70,6 +84,26 @@ type CreateRecordResponse struct {
 	RequestID string     `json:"RequestId"`
 }
 
+// A RecordContent represents the content of a
+// period of record.
+type RecordContent struct {
+	Duration        float64   `json:"Duration"`
+	OssEndpoint     string    `json:"OssEndpoint"`
+	EndTime         time.Time `json:"EndTime"`
+	StartTime       time.Time `json:"StartTime"`
+	OssObjectPrefix string    `json:"OssObjectPrefix"`
+	OssBucket       string    `json:"OssBucket"`
+}
+
+// DescribeContentResponse is the response for
+// DescribeLiveStreamRecordContent.
+type DescribeContentResponse struct {
+	RequestID             string `json:"RequestId"`
+	RecordContentInfoList struct {
+		RecordContentInfo []RecordContent `json:"RecordContentInfo"`
+	} `json:"RecordContentInfoList"`
+}
+
 // DescribeRecords uses the signer to send a DescribeRecordsAPI.
 func DescribeRecords(s aliyun.Signer, uri StreamURI, start, end time.Time) (resp DescribeRecordsResponse, err error) {
 	api := DescribeRecordsAPI(uri, start, end)
@@ -81,6 +115,14 @@ func DescribeRecords(s aliyun.Signer, uri StreamURI, start, end time.Time) (resp
 // CreateRecord uses the signer to send a CreateRecordAPI.
 func CreateRecord(s aliyun.Signer, uri StreamURI, start, end time.Time, oss aliyun.OSS) (resp CreateRecordResponse, err error) {
 	api := CreateRecordAPI(uri, start, end, oss)
+
+	err = aliyun.Get(http.DefaultClient, s, api, Host, &resp)
+	return
+}
+
+// DescribeRecordContent uses the signer to send a DescribeRecordsAPI.
+func DescribeRecordContent(s aliyun.Signer, uri StreamURI, start, end time.Time) (resp DescribeContentResponse, err error) {
+	api := DescribeRecordContentAPI(uri, start, end)
 
 	err = aliyun.Get(http.DefaultClient, s, api, Host, &resp)
 	return
